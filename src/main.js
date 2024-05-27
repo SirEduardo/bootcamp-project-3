b2e690e534acdb8afca49dc60379e70d70bffa1a
SirEduardo <eduardosaanchezlopez@gmail.com>
2024-05-27 12:20:21 +0200

    errors update

--- src/main.js
+++ src/main.js
@@ -42,12 +42,12 @@ header.appendChild(divLogin);
 
 const accessKey = "EGdKue5UuiKjVkBjOPUDckF_UyZY3ROjv28eWwUeNQU";
 const url = "https://api.unsplash.com/photos/random";
-const searchUrl = "https://api.unsplash.com/search/photos"
+const searchUrl = "https://api.unsplash.com/search/photos";
 
 const fetchPhotos = async () => {
   const photos = [];
   try {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 50; i++) {
       const response = await fetch(url, {
         headers: {
           Authorization: `Client-ID ${accessKey}`,
@@ -62,6 +62,30 @@ const fetchPhotos = async () => {
     printPhotos(photos);
   } catch (error) {
     console.log(error);
+    recomendedSearch()
+  }
+};
+
+const fetchCollection = async (searchQuery) => {
+  try {
+    const response = await fetch(`${searchUrl}?query=${searchQuery}`, {
+      headers: {
+        Authorization: `Client-ID ${accessKey}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    const collections = data.results.map((result) => result.urls.regular);
+    if (collections.length > 0){
+      
+    printPhotos(collections);
+    }else{
+      recomendedSearch()
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
@@ -69,50 +93,73 @@ const printPhotos = (photos) => {
   const imagenContainer = document.querySelector(".img-container");
   imagenContainer.innerHTML = "";
 
-  for (const photo of photos) {
-    const divPhoto = document.createElement("div");
-    divPhoto.classList.add("photo-container")
-    const imgElement = document.createElement("img");
-    imgElement.classList.add("photos")
-    imgElement.src = photo;
+  
+    for (const photo of photos) {
+      const divPhoto = document.createElement("div");
+      divPhoto.classList.add("photo-container");
+      const imgElement = document.createElement("img");
+      imgElement.classList.add("photos");
+      imgElement.src = photo;
 
-    divPhoto.appendChild(imgElement);
-    imagenContainer.appendChild(divPhoto);
+      divPhoto.appendChild(imgElement);
+      imagenContainer.appendChild(divPhoto);
+    }
+  
+
   }
-};
 
 
-const fetchCollection = async (searchQuery) => {
-  try{
-   
-      const response = await fetch (`${searchUrl}?query=${searchQuery}`, {
-        headers: {
-          Authorization: `Client-ID ${accessKey}`,
-        },
-      })
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-      }
-      const data = await response.json()
-      const collections = data.results.map(result => result.urls.regular)
-    printPhotos(collections)
-  } catch (error) {
-    console.log(error)
-  }
+const recomendedSearch = () =>{
+  const messagesContainer = document.createElement("div")
+  const errorMessage = document.createElement("p");
+  const recomendedMessage = document.createElement("p");
+  errorMessage.textContent = "No se han encontrado imagenes relacionadas";
+  recomendedMessage.textContent = "Busquedas recomendadas";
+
+  const btnContainer = document.createElement("div");
+  const btnRecomended1 = document.createElement("button");
+  const btnRecomended2 = document.createElement("button");
+  const btnRecomended3 = document.createElement("button");
+
+  messagesContainer.classList.add("message-container")
+  btnContainer.classList.add("btn-container");
+  btnRecomended1.classList.add("btn-recomended");
+  btnRecomended2.classList.add("btn-recomended");
+  btnRecomended3.classList.add("btn-recomended");
+
+  btnRecomended1.textContent = "Viajes";
+  btnRecomended2.textContent = "Decoracion";
+  btnRecomended3.textContent = "Playa";
+  
+
+  btnContainer.appendChild(btnRecomended1);
+  btnContainer.appendChild(btnRecomended2);
+  btnContainer.appendChild(btnRecomended3);
+  messagesContainer.appendChild(errorMessage);
+  messagesContainer.appendChild(recomendedMessage)
+  document.body.appendChild(messagesContainer);
+  document.body.appendChild(btnContainer);
+
+  const buttons = document.querySelectorAll(".btn-recomended");
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      input.value = button.textContent;
+      fetchCollection(button.textContent);
+    });
+  });
+
 }
-
 
 const handleSearch = (e) => {
-  e.preventDefault()
-  const search = input.value.trim()
-  if (search.length > 0){
-    fetchCollection(search)
-  }else
-  fetchPhotos()
-}
+  e.preventDefault();
+  const search = input.value.trim();
+  if (search.length > 0) {
+    fetchCollection(search);
+    console.log(search);
+  } else 
+  fetchPhotos();
+};
 
-input.addEventListener("submit",handleSearch)
-
-
+form.addEventListener("submit", handleSearch);
 
 fetchPhotos();
